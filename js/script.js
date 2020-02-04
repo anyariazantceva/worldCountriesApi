3cbb6c92e63102114cb04693054c065c6cfd1e31
Anna Ryazantceva <anyariazantzeva@yandex.ru>
2020-02-04 12:19:48 +0200

    Added sorting by capital, population

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ const startBtn = document.querySelector('.btn-start');
 const nameBtn = document.querySelector('.btn-name');
 const reverseBtn = document.querySelector('.btn-reverse');
 const searchInput = document.querySelector('.search__control');
+const countryNameBtn = document.querySelector('.btn-countryname');
+const capitalBtn = document.querySelector('.btn-capital');
+const populationBtn = document.querySelector('.btn-population');
+
 const list = 'https://restcountries.eu/rest/v2/all';
 
 function loadData () {
@@ -11,15 +15,19 @@ function loadData () {
         .then(countries => {
             showCountries(countries);
             showTotal(countries);
-            startBtn.addEventListener('click', () => {
+            countryNameBtn.addEventListener('click', () => {
                 searchInput.addEventListener('input', () => {
-                    renderSortedArray('start', countries);
+                    renderSortedArray('name', countries);
                 });
             });
-            nameBtn.addEventListener('click', () => {
+            capitalBtn.addEventListener('click', () => {
                 searchInput.addEventListener('input', () => {
-                    renderSortedArray('any', countries);
+                    renderSortedArray('capital', countries);
                 });
+            });
+            populationBtn.addEventListener('click', () => {
+                renderSortedArray('population', countries);
+
             });
             reverseBtn.addEventListener('click', () => {
                 reverseArray(countries)
@@ -43,6 +51,7 @@ function showCountries (array) {
         let imagePic = document.createElement('img');
         let textBlock = document.createElement('div');
         let capitalBlock = document.createElement('div');
+        let populationBlock = document.createElement('div');
         block.classList.add('countries__item');
         imageBlock.classList.add('countries__image');
         imagePic.classList.add('countries__pic');
@@ -50,9 +59,11 @@ function showCountries (array) {
         textBlock.classList.add('countries__name');
         textBlock.textContent = item.name;
         capitalBlock.classList.add('countries__capital');
-        capitalBlock.textContent = item.capital !=='' ? `Capital: ${item.capital}`: 'There is no capital'
+        capitalBlock.textContent = item.capital !=='' ? `Capital: ${item.capital}`: 'There is no capital';
+        populationBlock.classList.add('countries__population');
+        populationBlock.textContent = `Population: ${item.population}`;
         imageBlock.append(imagePic);
-        block.append(imageBlock, textBlock, capitalBlock);
+        block.append(imageBlock, textBlock, capitalBlock, populationBlock);
         container.append(block);
 
     })
@@ -60,30 +71,57 @@ function showCountries (array) {
 
 const renderSortedArray = (type, countries) => {
     let value = document.querySelector('.search__control').value;
-    let upperCasedValue = value.toUpperCase();
-    if(type === 'start') {
-        let sortedArray = sortStartLetter(countries, upperCasedValue);
+    if(type === 'name') {
+        let sortedArray = sortAnySymbols(countries, value);
         showCountries(sortedArray);
         showTotal(sortedArray);
         reverseBtn.addEventListener('click', () => {
             reverseArray(sortedArray)
         })
 
-    } else if(type === 'any') {
-        let sortedArray = sortAnySymbols(countries, value);
+    } else if(type === 'capital') {
+        let sortedArray = sortByCapital(countries, value);
         showCountries(sortedArray);
         showTotal(sortedArray);
         reverseBtn.addEventListener('click', () => {
             reverseArray(sortedArray)
         })
-    } else {
-        let sortedArray = sortAnySymbols(countries, value);
+    } else if(type === 'population') {
+        let active = 'up';
+        let sortedArray = active === 'up' ? sortByPopulationUp(countries, active) : sortByPopulationDown(countries, active);
         showCountries(sortedArray);
         showTotal(sortedArray);
         reverseBtn.addEventListener('click', () => {
             reverseArray(sortedArray)
         })
     }
+};
+
+const sortByPopulationDown = (arr, active) => {
+    let sortedArr = arr.sort((a, b) => {
+        return a.population - b.population
+    });
+
+    clearContainer();
+    active = 'up'
+    return sortedArr
+};
+const sortByPopulationUp = (arr, active) => {
+    let sortedArr = arr.sort((a, b) => {
+        return b.population - a.population
+    });
+    clearContainer();
+    active = 'down';
+    return sortedArr
+}
+
+const sortByCapital = (arr, match) => {
+    let sortedArr = arr.filter((item) => {
+       return item.capital.toLowerCase().startsWith(match)
+    });
+    clearContainer();
+    console.log(sortedArr);
+    return sortedArr
 };
 
 const sortAnySymbols = (arr, match) => {
