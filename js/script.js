9414fd062a5c4fc4443248cf02ed9d66e2db0d7d
Anna Ryazantceva <anyariazantzeva@yandex.ru>
2020-02-07 10:13:26 +0200

    Added chart using chart js and refactored code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 const container = document.querySelector('.countries');
-const startBtn = document.querySelector('.btn-start');
-const nameBtn = document.querySelector('.btn-name');
 const reverseBtn = document.querySelector('.btn-reverse');
 const searchInput = document.querySelector('.search__control');
 const countryNameBtn = document.querySelector('.btn-countryname');
 const capitalBtn = document.querySelector('.btn-capital');
 const populationBtn = document.querySelector('.btn-population');
+const chartBtn = document.querySelector('.btn-chart');
+let check;
 
 const list = 'https://restcountries.eu/rest/v2/all';
 
@@ -27,11 +27,17 @@ function loadData () {
             });
             populationBtn.addEventListener('click', () => {
                 renderSortedArray('population', countries);
-
             });
             reverseBtn.addEventListener('click', () => {
                 reverseArray(countries)
+            });
+            chartBtn.addEventListener('click', () => {
+                clearContainer();
+                showGraph(countries);
+
             })
+
+
         })
         .catch(error => console.log(error))
 }
@@ -77,43 +83,48 @@ const renderSortedArray = (type, countries) => {
         showTotal(sortedArray);
         reverseBtn.addEventListener('click', () => {
             reverseArray(sortedArray)
-        })
-
+        });
+        let ctx = document.getElementById('myChart');
+        ctx.innerHTML = '';
     } else if(type === 'capital') {
         let sortedArray = sortByCapital(countries, value);
         showCountries(sortedArray);
         showTotal(sortedArray);
         reverseBtn.addEventListener('click', () => {
             reverseArray(sortedArray)
-        })
+        });
+        let ctx = document.getElementById('myChart');
+        ctx.innerHTML = '';
     } else if(type === 'population') {
-        let active = 'up';
-        let sortedArray = active === 'up' ? sortByPopulationUp(countries, active) : sortByPopulationDown(countries, active);
+        let sortedArray = check ? sortByPopulationDown(countries) : sortByPopulationUp(countries);
         showCountries(sortedArray);
         showTotal(sortedArray);
         reverseBtn.addEventListener('click', () => {
-            reverseArray(sortedArray)
-        })
+            reverseArray(sortedArray);
+        });
+        let ctx = document.getElementById('myChart');
+        ctx.innerHTML = '';
     }
 };
 
-const sortByPopulationDown = (arr, active) => {
+const sortByPopulationDown = (arr) => {
+    check = false;
     let sortedArr = arr.sort((a, b) => {
         return a.population - b.population
     });
 
     clearContainer();
-    active = 'up'
     return sortedArr
 };
-const sortByPopulationUp = (arr, active) => {
+
+const sortByPopulationUp = (arr) => {
+    check = true;
     let sortedArr = arr.sort((a, b) => {
         return b.population - a.population
     });
     clearContainer();
-    active = 'down';
     return sortedArr
-}
+};
 
 const sortByCapital = (arr, match) => {
     let sortedArr = arr.filter((item) => {
@@ -142,19 +153,77 @@ const clearContainer = () => {
     container.innerHTML = '';
 };
 
-const sortStartLetter = (arr, match) => {
-  let sortedArr = arr.filter((item) => {
-      return item.name.toUpperCase().startsWith(match)
-  });
-  clearContainer();
-  return sortedArr
-};
-
 const showTotal = (arr) => {
     let total = document.querySelector('.search__total');
     total.textContent = `Total number of countries: ${arr.length}`
 };
 
+const pickPopulated = (arr) => {
+   let sortedArr = sortByPopulationUp(arr);
+   let tenCountries = [];
+   for(let i = 0; i<10; i++) {
+       tenCountries.push(sortedArr[i]);
+   }
+   return tenCountries
+};
+
+const showGraph = (arr) => {
+    let sortedArr = pickPopulated(arr);
+    let names = [];
+    let population = [];
+    let obj = {};
+
+    for(let i = 0; i < 10; i++) {
+        names.push(sortedArr[i].name);
+        population.push(sortedArr[i].population);
+    }
+
+    obj.names = names;
+    obj.population = population;
+    console.log(obj.names);
+    console.log(obj.population);
+    let ctx = document.getElementById('myChart').getContext('2d');
+    let myChart = new Chart(ctx, {
+        type: 'horizontalBar',
+        data: {
+            labels: obj.names,
+            datasets: [{
+                label: '10 Most Populated Countries',
+                data: obj.population,
+                backgroundColor: [
+                    'rgb(255,62,33)',
+                    'rgb(255,230,27)',
+                    'rgb(255,62,33)',
+                    'rgb(255,230,27)',
+                    'rgb(255,62,33)',
+                    'rgb(255,230,27)',
+                    'rgb(255,62,33)',
+                    'rgb(255,230,27)',
+                    'rgb(255,62,33)',
+                    'rgb(255,230,27)'
+
+
+                ],
+                borderColor: [
+                    'rgb(255,244,168)'
+                ],
+                borderWidth: 1,
+                barPercentage: 10,
+                barThickness: 30,
+                maxBarThickness: 30,
+                minBarLength: 2
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    type: 'category',
+                    labels: obj.names
+                }],
+            }
+        }
+    });
+};
 
 
 
